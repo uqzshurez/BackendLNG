feb4f659e8a1e27901a5646c5a2f2fe5381054f7
Leandro Gimenez <69045806+uqzshurez@users.noreply.github.com>
2023-05-13 15:01:58 -0300

    corrigiendo cors

--- src/main/java/com/portfolio/lngport/Controller/CHys.java
+++ src/main/java/com/portfolio/lngport/Controller/CHys.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200") //ACA DEJAR LISTO PARA HACER LA SUBIDA
+@CrossOrigin(origins = "https://portfolio-frontend-lng.web.app") //ACA DEJAR LISTO PARA HACER LA SUBIDA
 @RequestMapping("/skill")
 public class CHys {
 
