32c6e1d09202edaa7b5f7265691a0e8831210de2
Leandro Gimenez <69045806+uqzshurez@users.noreply.github.com>
2023-05-15 14:10:58 -0300

    editando

--- src/main/java/com/portfolio/lngport/LngportApplication.java
+++ src/main/java/com/portfolio/lngport/LngportApplication.java
@@ -2,14 +2,14 @@ package com.portfolio.lngport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LngportApplication {
 
-    
-	public static void main(String[] args) {
-		SpringApplication.run(LngportApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LngportApplication.class, args);
+    }
 }
--- src/main/java/com/portfolio/lngport/Repository/IPersonaRepository.java
+++ src/main/java/com/portfolio/lngport/Repository/IPersonaRepository.java
@@ -2,14 +2,13 @@
 package com.portfolio.lngport.Repository;
 
 import com.portfolio.lngport.Entity.Persona;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface IPersonaRepository extends JpaRepository <Persona,Integer>  {
-    public Optional<Persona> findByNombre(String nombre);
-    public boolean existsByNombre(String nombre);
+public interface IPersonaRepository extends JpaRepository <Persona,Long>  {
+    
     
 }
+
