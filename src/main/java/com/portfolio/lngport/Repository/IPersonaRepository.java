ea8941e8b41bbd1fbdd291d0232c808d11ecccbc
Leandro Gimenez <69045806+uqzshurez@users.noreply.github.com>
2023-05-14 23:47:31 -0300

    cambie de bd

--- src/main/java/com/portfolio/lngport/Repository/IPersonaRepository.java
+++ src/main/java/com/portfolio/lngport/Repository/IPersonaRepository.java
@@ -9,7 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IPersonaRepository extends JpaRepository <Persona,Long>  {
-    public Optional<Persona> findByNombre(String nombre);
-    public boolean existsByNombre(String nombre);
-    
+
 }
